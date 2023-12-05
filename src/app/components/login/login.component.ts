4f5d978345294f9f680f16a3dd4bdd14267ae1b1
Axel Mariano Bordon Alvarez <axel.mbordon@gmail.com>
2023-12-05 10:55:14 -0300

    Login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { Credentials } from 'src/app/models/login/Credentials';
 import { LoginResult } from 'src/app/models/results/login/LoginResult';
 import { UserService } from 'src/app/services/user.service';
@@ -10,28 +11,35 @@ import { UserService } from 'src/app/services/user.service';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {
+export class LoginComponent implements OnDestroy {
   credentials: Credentials = {
     email: '',
     password: '',
   };
+  subscriptions: Subscription = new Subscription();
   constructor(private userService: UserService, private router: Router) {}
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
   login(form: NgForm) {
     if (form.invalid) {
       alert('El email o contraseña no tienen el formato correcto');
     }
-    this.userService.login(this.credentials).subscribe({
-      next: (response: LoginResult) => {
-        if (response.ok) {
+    this.subscriptions.add(
+      this.userService.login(this.credentials).subscribe({
+        next: (response: LoginResult) => {
+          if (!response.ok) {
+            alert(response.error);
+            return;
+          }
           alert(response.message);
           localStorage.setItem('user_token', response.token);
           this.router.navigate(['home']);
-        }
-        alert(response.error);
-      },
-      error: (error: Error) => {
-        alert(error.message);
-      },
-    });
+        },
+        error: (error: Error) => {
+          alert(error.message);
+        },
+      })
+    );
   }
 }
